b4a1928ed6db66e4d7263e73617c072f420e96c9
Ipstenu (Mika Epstein) <Ipstenu@users.noreply.github.com>
2026-04-11 09:03:42 -0700

    Update basebelles.php

--- basebelles.php
+++ basebelles.php
@@ -3,7 +3,7 @@
  * Plugin Name: Base*Belles
  * Plugin URI:  https://github.com/Ipstenu/basebelles
  * Description: All the base code for Base*Belles - This controls all the blocks.
- * Version: 1.2.1
+ * Version: 1.2.2
  * Author: Ipstenu
  *
  * @package Base*Belles
