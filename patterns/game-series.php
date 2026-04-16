fd3ee3ff0389e4d19e2c31ac02a6ce869c44aa89
Mika Ipstenu Epstein <ipstenu@halfelf.org>
2026-04-16 15:55:09 -0700

    feat(patterns): add pattern registration for game series template in Basebelles class
    Signed-off-by: Mika Ipstenu Epstein <ipstenu@halfelf.org>

---
+++ patterns/game-series.php
@@ -0,0 +1,63 @@
+<!-- wp:paragraph -->
+<p>Intro</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Series Results</h2>
+<!-- /wp:heading -->
+
+<!-- wp:acf/basebelles-series {"name":"acf/basebelles-series","data":{"field_69c9b65a0d4e8":"0","field_69caeaeb128f1":{"field_69caebbe128f2":"20260416","field_69caebf0128f3":"20260416"},"field_69c9b6120d4e7":"tbd","field_69c9b67b0d4e9":"","field_69c9b6880d4ea":""},"mode":"preview"} /-->
+
+<!-- wp:paragraph -->
+<p>Series Notes</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Game Results</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Game 1 (X) - WONLOST</h3>
+<!-- /wp:heading -->
+
+<!-- wp:acf/basebelles-results {"name":"acf/basebelles-results","data":{"field_69c707ee1ddb3":{"field_69c7084e1ddb6":"1 - 0"},"field_69c708d15fdff":{"field_69c708d15fe00":"tbd","field_69c708d15fe04":"0 - 1"},"field_69c7087fc99cf":{"field_69c706cbee27f":"1","field_69c706bdee27e":"9","field_69c706e3ee280":{"field_69c70791ee281":"0","field_69c7079fee282":"0"},"field_69c707c05892a":{"field_69c707c05892b":"0","field_69c707c05892c":"0"},"field_69c707cedc16b":{"field_69c707cedc16c":"0","field_69c707cedc16d":"0"},"field_69c70d26cd56b":""}},"mode":"preview"} /-->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li>TBD</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:acf/basebelles-streamable {"name":"acf/basebelles-streamable","data":{"field_69d69d4716ab7":""},"mode":"preview"} /-->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Game 2 (X) - WONLOST</h3>
+<!-- /wp:heading -->
+
+<!-- wp:acf/basebelles-results {"name":"acf/basebelles-results","data":{"field_69c707ee1ddb3":{"field_69c7084e1ddb6":"1 - 0"},"field_69c708d15fdff":{"field_69c708d15fe00":"tbd","field_69c708d15fe04":"0 - 1"},"field_69c7087fc99cf":{"field_69c706cbee27f":"1","field_69c706bdee27e":"9","field_69c706e3ee280":{"field_69c70791ee281":"0","field_69c7079fee282":"0"},"field_69c707c05892a":{"field_69c707c05892b":"0","field_69c707c05892c":"0"},"field_69c707cedc16b":{"field_69c707cedc16c":"0","field_69c707cedc16d":"0"},"field_69c70d26cd56b":""}},"mode":"preview"} /-->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li>TBD</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:acf/basebelles-streamable {"name":"acf/basebelles-streamable","data":{"field_69d69d4716ab7":""},"mode":"preview"} /-->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Game 3 (X) - WONLOST</h3>
+<!-- /wp:heading -->
+
+<!-- wp:acf/basebelles-results {"name":"acf/basebelles-results","data":{"field_69c707ee1ddb3":{"field_69c7084e1ddb6":"1 - 0"},"field_69c708d15fdff":{"field_69c708d15fe00":"tbd","field_69c708d15fe04":"0 - 1"},"field_69c7087fc99cf":{"field_69c706cbee27f":"1","field_69c706bdee27e":"9","field_69c706e3ee280":{"field_69c70791ee281":"0","field_69c7079fee282":"0"},"field_69c707c05892a":{"field_69c707c05892b":"0","field_69c707c05892c":"0"},"field_69c707cedc16b":{"field_69c707cedc16c":"0","field_69c707cedc16d":"0"},"field_69c70d26cd56b":""}},"mode":"preview"} /-->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li>TBD</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:acf/basebelles-streamable {"name":"acf/basebelles-streamable","data":{"field_69d69d4716ab7":""},"mode":"preview"} /-->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
